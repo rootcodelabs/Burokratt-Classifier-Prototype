a6113aff7a9eb179494d4609554aceccd1f07ae0
pamodaDilranga <pdrox9@gmail.com>
2024-02-19 02:48:19 +0530

    albert base model

--- nlp_models/albert.py
+++ nlp_models/albert.py
@@ -1,12 +1,11 @@
 from sklearn.metrics import accuracy_score, f1_score
 from sklearn.preprocessing import LabelEncoder
-from sklearn.model_selection import train_test_split
 from transformers import AlbertTokenizer, AlbertForSequenceClassification, Trainer, TrainingArguments
 import torch
 
-class ALBERTModel:
-    def __init__(self, model_path):
-        self.model_path = model_path
+class ALBERTTrainer:
+    def __init__(self, model_id):
+        self.model_path = f'nlp_models/{model_id}/'
         self.model = None
         self.tokenizer = AlbertTokenizer.from_pretrained('albert-base-v2')
         self.label_encoder = LabelEncoder()
@@ -20,18 +19,15 @@ class ALBERTModel:
         return tokenized_inputs
 
     def train(self, X_train, y_train, X_test, y_test):
-        # Preprocess data
+
         X_train, y_train = self._preprocess_data(X_train, y_train)
         X_test, y_test = self._preprocess_data(X_test, y_test)
 
-        # Tokenize data
         train_inputs = self._tokenize_data(X_train)
         test_inputs = self._tokenize_data(X_test)
 
-        # Define model
         self.model = AlbertForSequenceClassification.from_pretrained('albert-base-v2', num_labels=len(set(y_train)))
 
-        # Training arguments
         training_args = TrainingArguments(
             per_device_train_batch_size=8,
             per_device_eval_batch_size=8,
@@ -48,7 +44,6 @@ class ALBERTModel:
             metric_for_best_model='accuracy'
         )
 
-        # Trainer
         trainer = Trainer(
             model=self.model,
             args=training_args,
@@ -58,28 +53,37 @@ class ALBERTModel:
                                         'f1_score': f1_score(p.predictions.argmax(-1), p.label_ids, average='weighted')}
         )
 
-        # Train the model
         trainer.train()
 
-        # Save model
         self.model.save_pretrained(self.model_path)
 
-        # Evaluate the model
         evaluation_results = trainer.evaluate(eval_dataset=torch.utils.data.TensorDataset(test_inputs['input_ids'], test_inputs['attention_mask'], torch.tensor(y_test)))
 
         return evaluation_results['accuracy'], evaluation_results['f1_score']
 
-# Example usage:
-if __name__ == "__main__":
-    X_train = ["example text 1", "example text 2", "example text 3"]
-    y_train = ["label1", "label2", "label3"]
-    X_test = ["example text 4", "example text 5"]
-    y_test = ["label1", "label2"]
+class ALBERTClassifier:
+    def __init__(self, model_id):
+        self.model_path = f'nlp_models/{model_id}/'
+        self.model = None
+        self.tokenizer = AlbertTokenizer.from_pretrained('albert-base-v2')
+        self.label_encoder = LabelEncoder()
 
-    model_path = "./albert_model"
+    def _tokenize_data(self, X):
+        tokenized_inputs = self.tokenizer(X, padding=True, truncation=True, return_tensors="pt")
+        return tokenized_inputs
 
-    albert_model = ALBERTModel(model_path)
-    accuracy, f1_score = albert_model.train(X_train, y_train, X_test, y_test)
+    def load_model(self):
+        if self.model is None:
+            self.model = AlbertForSequenceClassification.from_pretrained(self.model_path)
 
-    print("Accuracy:", accuracy)
-    print("F1 Score:", f1_score)
+    def classify_text(self, text):
+        self.load_model()
+
+        inputs = self._tokenize_data([text])
+
+        with torch.no_grad():
+            outputs = self.model(**inputs)
+            predicted_class_idx = outputs.logits.argmax().item()
+
+        predicted_class = self.label_encoder.inverse_transform([predicted_class_idx])[0]
+        return predicted_class
