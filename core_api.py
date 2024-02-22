6c9691acd2843cd3041d09b13ba9451dab0fe1e8
pamodaDilranga <pdrox9@gmail.com>
2024-02-23 00:19:20 +0530

    change api to localhost

--- core_api.py
+++ core_api.py
@@ -486,4 +486,4 @@ def classify_text(data : ClassifyTextData):
         raise HTTPException(status_code=500, detail=str(e))
 
 if __name__ == "__main__":
-    uvicorn.run(app, host="0.0.0.0", port=8000)
+    uvicorn.run(app, host="localhost", port=8000)
